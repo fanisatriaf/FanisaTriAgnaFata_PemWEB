8aa8a53f335a80a6811923e904143bd93057b1b7
Fanisa Tri Agna Fata <agnafatafanisatri@gmail.com>
2024-10-24 08:31:33 +0700

    Biodata

---
+++ script.js
@@ -0,0 +1,7 @@
+ // Get the button element
+    const saveBtn = document.getElementById('save-btn');
+
+    // Add an event listener to the button
+    saveBtn.addEventListener('click', function() {
+        alert('Data berhasil disimpan!');
+    });
